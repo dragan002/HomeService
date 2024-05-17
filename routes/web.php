132387191033ebcac230964ffdefbad25bba2fbf
dragan002 <draganvujic29@gmail.com>
2024-05-17 13:05:39 +0200

    implement Home page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\HomeComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', HomeComponent::class)->name('home');
