c140ab974e8f59e41009b3a7810abff7f5d2a774
dragan002 <draganvujic29@gmail.com>
2024-05-29 12:14:13 +0200

    Creating a Slider
    *firstly added php artisan make:livewire admim/AdminSliderComponent
    
    After that I add 2 more component for edit and add slider

---
+++ app/Livewire/Admim/AdminSliderComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admim;
+
+use Livewire\Component;
+
+class AdminSliderComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admim.admin-slider-component');
+    }
+}
---
+++ app/Livewire/Admin/AdminAddSlideComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminAddSlideComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-add-slide-component');
+    }
+}
