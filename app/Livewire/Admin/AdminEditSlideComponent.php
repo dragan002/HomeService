084f273140dccd484cd94845ff27212b9dd6c696
dragan002 <draganvujic29@gmail.com>
2024-05-30 09:38:22 +0200

    Edit Sliders inprogress

--- app/Livewire/Admin/AdminEditSlideComponent.php
+++ app/Livewire/Admin/AdminEditSlideComponent.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Slider;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class AdminEditSlideComponent extends Component
 {
+    use WithFileUploads;
+
+    public $slide_id;
+    public $title;
+    public $image;
+    public $status;
+    public $newImage;
+
+    public function mount($slide_id) {
+        $sliders = Slider::find($slide_id);
+        $this->slide_id = $sliders->id;
+        $this->title = $sliders->title;
+        $this->image = $sliders->image;
+        $this->status = $sliders->status;
+    }
+
     public function render()
     {
         return view('livewire.admin.admin-edit-slide-component')->layout('layout.base');
