9d9da9b56540ffb6bf18a3a9bb36f9deaf730ff4
dragan002 <draganvujic29@gmail.com>
2024-05-31 13:03:13 +0200

    Slider is visible on Home Page

--- app/Livewire/HomeComponent.php
+++ app/Livewire/HomeComponent.php
@@ -5,6 +5,8 @@ namespace App\Livewire;
 use App\Models\Service;
 use Livewire\Component;
 use App\Models\ServiceCategory;
+use App\Models\Slider;
+
 
 class HomeComponent extends Component
 {
@@ -15,10 +17,12 @@ class HomeComponent extends Component
         $fscategories = ServiceCategory::where('featured', 1)->inRandomOrder()->take(8)->get();
         $sid = ServiceCategory::whereIn('slug',['ac','tv', 'refrigerator', 'geyser','water-purifier'])->get()->pluck('id');
         $aservices = Service::whereIn('service_category_id', $sid)->inRandomOrder()->take(8)->get();
+        $slides = Slider::where('status',1)->get();
         return view('livewire.home-component',[ 'scategories'=> $scategories,
                                                 'fservices' => $fservices,
                                                 'fscategories'=>$fscategories,
-                                                'aservices'=>$aservices])
+                                                'aservices'=>$aservices,
+                                                'slides'=>$slides])
                                                 ->layout('layout.base');
     }
 }
