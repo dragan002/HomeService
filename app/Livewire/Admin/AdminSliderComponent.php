a9cad0cae11fa91dc0bd30818420304f2fbf5714
dragan002 <draganvujic29@gmail.com>
2024-05-31 12:34:16 +0200

    Delete Slide working as excepted, check for improvments

--- app/Livewire/Admin/AdminSliderComponent.php
+++ app/Livewire/Admin/AdminSliderComponent.php
@@ -9,6 +9,15 @@ use Livewire\WithPagination;
 class AdminSliderComponent extends Component
 {
     use WithPagination;
+
+    public function deleteSlide($id) {
+        $slide = Slider::find($id);
+        if(file_exists('images/slider' . '/' . $slide->image)) {
+            unlink('images/slider' . '/' . $slide-image);
+        }
+        $slide->delete();
+        session()->flash('message', 'Slide has been deleted successfully');
+    }
     public function render()
     {
         $slides = Slider::paginate(10);
