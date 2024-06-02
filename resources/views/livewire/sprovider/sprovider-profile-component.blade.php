fb09326eb2513ea0a22ec27323d67f11d1158018
dragan002 <draganvujic29@gmail.com>
2024-06-02 18:34:33 +0200

    Sprovider layout

--- resources/views/livewire/sprovider/sprovider-profile-component.blade.php
+++ resources/views/livewire/sprovider/sprovider-profile-component.blade.php
@@ -1,3 +1,59 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="section-title-01 honmob">
+        <div class="bg_parallax image_02_parallax"></div>
+        <div class="opacy_bg_02">
+            <div class="container">
+                <h1>Profile</h1>
+                <div class="crumbs">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li>/</li>
+                        <li>Profile</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="content-central">
+        <div class="content_info">
+            <div class="paddings-mini">
+                <div class="container">
+                    <div class="row portfolioContainer">
+                        <div class="col-md-12 col-md-offest-2 profile1">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <div class="col-md-6">
+                                        Profile
+                                    </div>
+                                    <div class="col-md-6">
+
+                                    </div>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            @if($sprovider->image)
+                                                <img src="{{ asset('images/sproviders') }}/{{ $sprovider->image }}" width="100%">
+                                            @else
+                                                <img src="{{ asset('images/sproviders/default.png') }}" width="100%">
+                                            @endif
+                                        </div>
+                                        <div class="col-md-8">
+                                            <h3>Name: {{ Auth::user()->name }}</h3>
+                                            <p>{{ $sprovider->about }}</p>
+                                            <p><b>Email:</b>{{ Auth::user()->email }}</p>
+                                            <p><b>Phone:</b>{{ Auth::user()->phone }}</p>
+                                            <p><b>City: {{ $sprovider->city }}</b></p>
+                                            <p><b>Service Category: {{ $sprovider->category->name }}</b></p>
+                                            <p><b>Service Location: {{ $sprovider->service_locations }}</b></p>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </div>
