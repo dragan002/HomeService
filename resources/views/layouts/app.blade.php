e5be59af6b8372588c66becf5f4848a13b7bde8e
dragan002 <draganvujic29@gmail.com>
2024-06-24 22:02:18 +0200

    .rest/test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         {{-- Icons font awsome --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
