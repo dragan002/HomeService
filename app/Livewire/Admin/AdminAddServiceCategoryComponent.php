1c315f2afc788f33baccceda6a2db580229df6ef
dragan002 <draganvujic29@gmail.com>
2024-07-01 21:29:25 +0200

    AdminAddServiceCategoryComponent breaking in smaller tasks

--- app/Livewire/Admin/AdminAddServiceCategoryComponent.php
+++ app/Livewire/Admin/AdminAddServiceCategoryComponent.php
@@ -17,34 +17,46 @@ class AdminAddServiceCategoryComponent extends Component
     public $slug;
     public $image;
 
-    public function generateSlug() {
+    public function generateSlug(): void {
         $this->slug = Str::slug($this->name, '-');
     }
 
-    public function update() {
-        $this->validateOnly([
-            'name' => 'required',
-            'slug' => 'required',
-            'image' => 'required|mimes:jpeg,png'
-         ]);
-    }
+   public function createNewCategory() {
+    $this->validate([
+        'name' => 'required',
+        'slug' => 'required',
+        'image' => 'required|mimes:jpeg,png'
+    ]);
 
-    public function createNewCategory() {
-        $this->validate([ 
-            'name' => 'required',
-            'slug' => 'required',
-            'image' => 'required|mimes:jpeg,png'
-        ]);
-
-        $scategory = new ServiceCategory();
-        $scategory->name = $this->name;
-        $scategory->slug = $this->slug;
-        $imageName = Carbon::now()->timestamp . '.' . $this->image->extension();
-        $this->image->storeAs('categories', $imageName);
-        $scategory->image = $imageName;
-        $scategory->save();
+    try {
+        $serviceCategory = $this->createServiceCategory();
+        $imageName = $this->uploadImage();
+        $this->saveCategory($serviceCategory, $imageName);
         session()->flash('message', 'Category has been created successfully');
+    } catch (\Exception $e){
+        \Log::error($e->getMessage());
+        session()->flash('error', 'An error occurred while creating the category. Please check AdminAddServiceCategoryComponent');
     }
+   }
+
+   private function createServiceCategory() {
+    
+    $serviceCategory = new ServiceCategory();
+    $serviceCategory->name = $this->name;
+    $serviceCategory->slug = $this->slug;
+    return $serviceCategory;
+   }
+
+   public function uploadImage() {
+    $imageName = Carbon::now()->timestamp . '.' . $this->image->extension();
+    $this->image->storeAs('categories', $imageName);
+    return $imageName;
+   }
+
+   public function saveCategory(ServiceCategory $serviceCategory, string $imageName) {
+    $serviceCategory->image = $imageName;
+    $serviceCategory->save();
+   }
 
     public function render()
     {
